23ff5c0605d975170fcd763fbf8ea349698d0537
Emmanuella Animwaa Kumah <emmanuellaanimwaa17@icloud.com>
2026-04-04 16:59:58 +0000

    created project structure

---
+++ src/main/java/com/emmanuella/ElupstraOS/ElupstraOsApplication.java
@@ -0,0 +1,13 @@
+package com.emmanuella.ElupstraOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElupstraOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElupstraOsApplication.class, args);
+	}
+
+}
